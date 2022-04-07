1273aa64989fef23e20e7f6dba691e793d597f01
mnicolas94 <miguelcok27@gmail.com>
2022-04-07 10:29:55 -0400

    FIX: remove ClassSelector argument from ClassSelectorSettingDrawer constructor BUILD: update setup.py and publish_on_pypi.yml to support better version bumping strategy mnicolas94 25 minutes ago

--- pyrulo_qt/selector_setting_drawer.py
+++ pyrulo_qt/selector_setting_drawer.py
@@ -7,9 +7,11 @@ from pyrulo_qt.selector_setting_type import ClassSelector
 
 class ClassSelectorSettingDrawer(SettingDrawer):
 
-    def __init__(self, selector: ClassSelector, setting_owner, setting: Setting):
+    def __init__(self, setting_owner, setting: Setting):
+        if not isinstance(setting.setting_type, ClassSelector):
+            raise TypeError(f"Setting type {setting.setting_type} is not of type ClassSelector")
         super(ClassSelectorSettingDrawer, self).__init__(setting_owner, setting)
-        self._selector = selector
+        self._selector: ClassSelector = setting.setting_type
 
         self._selector_widget = DrawerConfigurableSelector(self, base_class=self._selector.base_class)
         self._selector_widget.eventObjectSelected.connect(self._on_object_selected)
